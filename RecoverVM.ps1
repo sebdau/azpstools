4c5a973a0cb4779539ddb4184a33cb607836699b
sebdau <sebastian_dau@web.de>
2015-07-30 16:42:11 +0200

    V 0.1
    supporting premium storage VM recovery

---
+++ RecoverVM.ps1
@@ -0,0 +1,37 @@
+﻿$SubscriptionId = '7fb3973d-3253-47d7-bc3e-456c3a0b33f7'
+$StorageAccountName = 'sebdauvs15' 
+$ServiceName = 'sbdauvs15-recover'
+$VMName = 'sebdauvs15' 
+$RecoveryAdmin = 'recoveryAdmin'
+$RecoveryPW = 'LetMeInNow123'
+
+$RecoveryVMName = 'RC' + $(get-date -f yyMMddHHmm)
+$VMExportPath = $env:TEMP + '\' + $ServiceName + '_'  + $VMName + '.xml'
+
+$RecoveryImage =  Get-AzureVMImage |where ImageFamily -eq 'Windows Server 2012 R2 Datacenter'  | sort PublishedDate -Descending | select  -First 1
+
+#set subscription and premium storage account where os disk is 
+Set-AzureSubscription -SubscriptionId $SubscriptionId -CurrentStorageAccountName $StorageAccountName 
+
+#get defect vm object and export cfg to disk (temp)
+$vm = Get-AzureVM -ServiceName $ServiceName -Name $VMName
+$vm | Export-AzureVM -Path $VMExportPath
+Write-Host 'Original VM Configuration written to ' $VMExportPath -ForegroundColor Yellow
+
+#create new recovery in size of the defect vm (will create premium storage vm if needed)
+$recoveryVM = New-AzureQuickVM -WaitForBoot -Windows -ServiceName $ServiceName -Name $RecoveryVMName -InstanceSize $vm.InstanceSize -AdminUsername $RecoveryAdmin -Password $RecoveryPW -ImageName $RecoveryImage.ImageName 
+
+#once the recovery vm has booted in the same cloud service (to prevent VIP loss) we get rid of the defect vm (but keep disks)
+Remove-AzureVm -ServiceName $ServiceName -Name $VMName 
+
+#now we need to wait for a few secs until the os disk was released
+Start-Sleep -Seconds 60 | Out-Null
+
+#than we try to add the 
+$recoveryVM = Get-AzureVM -ServiceName $ServiceName -Name $RecoveryVMName
+$recoveryVM | Add-AzureDataDisk -Import -DiskName $vm.VM.OSVirtualHardDisk.DiskName  -LUN 0 | Update-AzureVM 
+
+Write-Output 'you can now connect remote desktop to the recovery vm ' $RecoveryVMName ' and fix issues on drive f: which is the os disk of the temp deleted vm ' $VMName
+Write-Output 'once you are done please setup and run recreatevm.ps'
+
+
---
+++ RecreateVM.ps1
@@ -0,0 +1,55 @@
+﻿$SubscriptionId = '7fb3973d-3253-47d7-bc3e-456c3a0b33f7'
+$StorageAccountName = 'sebdauvs15' 
+$OSDiskName = 'sebdauvs15-sebdauvs15-os-1438250832440'
+$DestServiceName = 'sbdauvs15-recover'
+#path to vm export xml (usually created while running recoverVM.ps1)
+$VMExportPath = $env:TEMP + '\' + $DestServiceName + '_'  + $DestVMName + '.xml'
+
+#default values only used when vm export path can not be imported as vm
+$DestVMName = 'sebdauvs15'
+$DestVMSize = 'Standard_DS2' 
+$PublicRdpPort = 3389
+$SubNetNames = '' #"PubSubnet","PrivSubnet" #leave empty if not required
+$DataDiskNames = $null #'sebdauvs15-sebdauvs15-data-1'. 'sebdauvs15-sebdauvs15-data-1' #leave empty if not required
+###################################################################################################################
+
+#set subscription and premium storage account where os disk is 
+Set-AzureSubscription -SubscriptionId $SubscriptionId -CurrentStorageAccountName $StorageAccountName
+
+#get location from storage account
+$location = (Get-AzureStorageAccount -StorageAccountName $StorageAccountName).Location
+
+#get the unattached os disk
+$OSDisk = Get-AzureDisk -DiskName $OSDiskName
+if ( $OSDisk.AttachedTo ) #asuming this disk is attached to recovery vm
+{
+    $attachedVm = Get-AzureVM -ServiceName $OSDisk.AttachedTo.HostedServiceName -Name $OSDisk.AttachedTo.RoleName
+    $attachedVm | Remove-AzureDataDisk -LUN 0 | Update-AzureVM
+    Start-Sleep -Seconds 30
+} 
+
+try
+{
+   $vm = Import-AzureVM   -Path $VMExportPath    
+}
+catch
+{
+
+    #prepare the 
+    $vm = New-AzureVMConfig -Name $DestVMName -InstanceSize $DestVMSize -DiskName $OSDiskName
+
+    #Attached the data disks to the new VM
+    foreach ($dataDiskName in $DataDiskNames)
+    {    
+        $vm | Add-AzureDataDisk -DiskName $DataDiskName
+    } 
+
+    # Edit this if you want to add more custimization to the new VM
+    $vm | Add-AzureEndpoint -Protocol tcp -LocalPort 3389 -PublicPort $PublicRdpPort -Name 'Rdp'
+    if ( ! [string]::IsNullOrEmpty($SubNetNames ))
+    {
+        $vm | Set-AzureSubnet $SubNetNames
+    }
+}
+
+New-AzureVM -ServiceName $DestServiceName -VMs $vm -Location $location -WaitForBoot
